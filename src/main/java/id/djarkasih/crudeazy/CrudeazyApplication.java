04a295367285fd9288c7748d3d764d19d137cf3f
Ahmad R. Djarkasih <djarkasih@gmail.com>
2020-01-22 21:35:10 +0700

    Add Spring Boot Rest skeleton project.

---
+++ src/main/java/id/djarkasih/crudeazy/CrudeazyApplication.java
@@ -0,0 +1,13 @@
+package id.djarkasih.crudeazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudeazyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudeazyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/djarkasih/crudeazy/CrudeazyApplicationTests.java
@@ -0,0 +1,13 @@
+package id.djarkasih.crudeazy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudeazyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
